9ff0d8cae3a957a381e26551440b843ae78f530e
snowkite985 <snowkite@outlook.com>
2013-04-07 22:08:57 +0800

    version v1.0

---
+++ ICDMS/Excel2Xml/BuildXml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Excel2Xml
+{
+    public class BuildXml
+    {
+        public List<Row> Rows { get; private set; }
+
+        public BuildXml(List<Row> rows)
+        {
+            this.Rows = rows;
+        }
+
+        List<Command> commandList = new List<Command>();
+
+        public void SetXml()
+        {
+            for (int i = 0; i < Rows.Count; i++)
+            {
+                if (Convert.ToInt32(Rows[i].RowID) <3)
+                {
+                    continue;
+                }
+
+                commandList.Add(new Command
+                {
+                    ID = Convert.ToInt32(Rows[i].RowID),
+                    Cells = Rows[i].Cells
+                });
+            }
+
+            XElement doc = new XElement("Commands");
+
+            doc.Save("result1.xml");
+
+            StringBuilder sb = new StringBuilder();
+
+            Dictionary<string, string> collapseList = new Dictionary<string,string>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (commandList[i].Cells.Count() > 7 && sb.Length >0)
+                {
+                    collapseList.Add(sb.ToString().Split(';').FirstOrDefault(), sb.ToString());
+                    sb.Remove(0, sb.Length);
+                    sb.Append(commandList[i].Cells[0].Data + ";");
+                }
+                else
+                {
+                    sb.Append(commandList[i].Cells[0].Data + ";");
+                }
+
+                doc.Add(new XElement("Command",
+                     new XAttribute("id", commandList[i].Cells[0].Data),
+                     new XElement("Address", commandList[i].Cells[1].Data),
+                     new XElement("Inputs",
+                         new XElement("Input",
+                             new XAttribute("name",commandList[i].Cells[2].Data),
+                             new XAttribute("Code",commandList[i].Cells[3].Data),
+                             new XAttribute("Type",commandList[i].Cells[4].Data))),
+                     new XElement("Condition",
+                         new XAttribute("statement", commandList[i].Cells[5].Data),
+                         new XAttribute("Value", commandList[i].Cells[6].Data))));
+
+                doc.Save("result1.xml");
+            }
+
+            //最后一次循环保存的值
+            if (sb.Length>0)
+            {
+                collapseList.Add(sb.ToString().Split(';').FirstOrDefault(), sb.ToString());
+                sb.Remove(0, sb.Length);
+            }
+
+            //生成合并项
+            foreach (var command in commandList.Where(o => o.Cells.Count() > 7))
+            {
+                if (command.Cells[0].Data != "EOF")
+                {
+                    doc.LastNode.AddAfterSelf(new XElement("Collpase",
+                          new XElement("CommandID", collapseList[command.Cells[0].Data]),
+                          new XElement("Action", command.Cells[7].Data),
+                          new XElement("Address", command.Cells[8].Data),
+                          new XElement("OutPut",
+                              new XAttribute("Name", command.Cells[9].Data),
+                              new XAttribute("Code", command.Cells[10].Data),
+                              new XAttribute("Type", command.Cells[11].Data),
+                              new XAttribute("Value", command.Cells[12].Data)
+                              )
+                       ));
+                    doc.Save("result1.xml");
+                }
+            }
+        }
+    }
+
+    public class Command{
+        public int ID { get; set; }
+
+        public Cell[] Cells { get; set; }
+    }
+
+    public class Input{
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string Statement { get; set; }
+    }
+
+    public class Output{
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Type { get; set; }
+
+        public string  Value {get;set;}
+    }
+}
--- ICDMS/Excel2Xml/Program.cs
+++ ICDMS/Excel2Xml/Program.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO.Packaging;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using System.Data;
 
 namespace Excel2Xml
 {
@@ -9,7 +14,151 @@ namespace Excel2Xml
     {
         static void Main(string[] args)
         {
+            List<Cell> parsedCells = new List<Cell>();
 
+            List<Row> parsedRows = new List<Row>();
+            //string fileName = @"C:\Git\Projects\ICDMS\aaa.xlsx";
+            string fileName = Path.Combine(Environment.CurrentDirectory, "aaa.xlsx");
+            Package xlsxPackage = Package.Open(fileName, FileMode.Open, FileAccess.ReadWrite);
+            try
+            {
+                PackagePartCollection allParts = xlsxPackage.GetParts();
+
+                PackagePart sharedStringsPart = (from part in allParts
+                                                 where part.ContentType.Equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml")
+                                                 select part).Single();
+
+                XElement sharedStringsElement = XElement.Load(XmlReader.Create(sharedStringsPart.GetStream()));
+
+                Dictionary<int, string> sharedStrings = new Dictionary<int, string>();
+                ParseSharedStrings(sharedStringsElement, sharedStrings);
+
+                XElement worksheetElement = GetWorksheet(1, allParts);
+
+                IEnumerable<XElement> cells = from c in worksheetElement.Descendants(ExcelNamespaces.excelNamespace + "c")
+                                              select c;
+
+                foreach (XElement cell in cells)
+                {
+                    string cellPosition = cell.Attribute("r").Value;
+                    int index = IndexOfNumber(cellPosition);
+                    string column = cellPosition.Substring(0, index);
+                    int row = Convert.ToInt32(cellPosition.Substring(index, cellPosition.Length - index));
+                    if (cell.HasElements)
+                    {
+                        if (cell.Attribute("t") != null && cell.Attribute("t").Value == "s")
+                        {
+                            // Shared value
+                            int valueIndex = Convert.ToInt32(cell.Descendants(ExcelNamespaces.excelNamespace + "v").Single().Value);
+                            parsedCells.Add(new Cell(column, row, sharedStrings[valueIndex]));
+                        }
+                        else
+                        {
+                            string value = cell.Descendants(ExcelNamespaces.excelNamespace + "v").Single().Value;
+                            parsedCells.Add(new Cell(column, row, value));
+                        }
+                    }
+                    else
+                    {
+                        parsedCells.Add(new Cell(column, row, ""));
+                    }
+                }
+
+
+                IEnumerable<XElement> rows = worksheetElement.Descendants(ExcelNamespaces.excelNamespace + "row");
+
+                foreach (XElement row in rows)
+                {
+                    string rowID = row.Attribute("r").Value;
+
+                    if (row.HasElements)
+                    {
+                        parsedRows.Add(new Row { RowID = rowID, Cells = parsedCells.Where(c => string.Compare(string.Format("{0}", c.Row), rowID, true) == 0 && !string.IsNullOrEmpty(c.Data)).ToArray() });
+                    }
+                }
+            }
+            finally
+            {
+                xlsxPackage.Close();
+            }
+
+
+            BuildXml bm = new BuildXml(parsedRows);
+            bm.SetXml();
+            //From here is additional code not covered in the posts, just to show it works
+            //foreach (Cell cell in parsedCells)
+            //{
+            //    Console.WriteLine(cell);
+            //    Console.ReadLine();
+            //}
         }
+
+        private static void ParseSharedStrings(XElement SharedStringsElement, Dictionary<int, string> sharedStrings)
+        {
+            IEnumerable<XElement> sharedStringsElements = from s in SharedStringsElement.Descendants(ExcelNamespaces.excelNamespace + "t")
+                                                          select s;
+
+            int Counter = 0;
+            foreach (XElement sharedString in sharedStringsElements)
+            {
+                sharedStrings.Add(Counter, sharedString.Value);
+                Counter++;
+            }
+        }
+
+        private static XElement GetWorksheet(int worksheetID, PackagePartCollection allParts)
+        {
+            PackagePart worksheetPart = (from part in allParts
+                                         where part.Uri.OriginalString.Equals(String.Format("/xl/worksheets/sheet{0}.xml", worksheetID))
+                                         select part).Single();
+
+            return XElement.Load(XmlReader.Create(worksheetPart.GetStream()));
+        }
+
+        private static int IndexOfNumber(string value)
+        {
+            for (int counter = 0; counter < value.Length; counter++)
+            {
+                if (char.IsNumber(value[counter]))
+                {
+                    return counter;
+                }
+            }
+
+            return 0;
+        }   
+    }
+
+    internal static class ExcelNamespaces
+    {
+        internal static XNamespace excelNamespace = XNamespace.Get("http://schemas.openxmlformats.org/spreadsheetml/2006/main");
+        internal static XNamespace excelRelationshipsNamepace = XNamespace.Get("http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+    }
+
+    public class Cell
+    {
+        public Cell(string column, int row, string data)
+        {
+            this.Column = column;
+            this.Row = row;
+            this.Data = data;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1} - {2}", Row, Column, Data);
+        }
+
+        public string Column { get; set; }
+        public int Row { get; set; }
+        public string Data { get; set; }
+    }
+
+
+    public class Row
+    {
+        public string RowID { get; set; }
+
+        public Cell[] Cells { get; set; }
     }
 }
